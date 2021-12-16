de2f7c5af19a779ef5784aa128a610fa1db21306
Ricardo Coronel <montotox@gmail.com>
2021-12-16 15:16:15 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [apiData, setApiData] = useState({});
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=malaga&appid=0e617da6a7a588ca043db63ccdb1f64d"
+    )
+      .then((response) => response.json())
+      .then((data) => setApiData(data));
+  }, []);
+  console.log(apiData);
   return (
     <div className="App">
       <header className="App-header">
